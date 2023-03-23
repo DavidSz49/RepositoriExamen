3428356f5aa456dc80f0bcdae0971da17235b263
DavidSz49 <127231203+DavidSz49@users.noreply.github.com>
2023-03-23 18:14:46 +0100

    Create App.java

---
+++ App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Soc David Santacruz i estic fent el examen de la UF2 del M01");
+    }
+}
